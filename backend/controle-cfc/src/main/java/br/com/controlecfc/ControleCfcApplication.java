1e9a7828b478a3587768569072013e20d4b67b33
luscaer <lerc.rj@hotmail.com>
2025-12-24 12:53:27 -0300

    feat: estrutura inicial do backend com Spring Boot e PostgreSQL

---
+++ backend/controle-cfc/src/main/java/br/com/controlecfc/ControleCfcApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlecfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleCfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleCfcApplication.class, args);
+	}
+
+}
---
+++ backend/controle-cfc/src/test/java/br/com/controlecfc/ControleCfcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controlecfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleCfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
