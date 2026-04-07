071e532f3610a071b0c046fdb6cc186c6727a3d1
luscaer <lerc.rj@hotmail.com>
2026-04-07 13:02:48 -0300

    feat(#1): Finalização da tokenixação JWT e criação de service de autenticação

--- backend/controle-cfc/src/main/java/br/com/controlecfc/security/jwt/JwtAuthenticationFilter.java
+++ backend/controle-cfc/src/main/java/br/com/controlecfc/security/jwt/JwtAuthenticationFilter.java
@@ -2,15 +2,19 @@ package br.com.controlecfc.security.jwt;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import br.com.controlecfc.security.UsuarioDetailsService;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +22,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
     private final JwtService jwtService;
     private final UsuarioDetailsService usuarioDetailsService;
@@ -53,8 +59,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }
-        } catch (UsernameNotFoundException e) {
-            e.printStackTrace();
+        } catch (JwtException e) {
+            logger.warn("Falha na autenticação JWT: {}", e.getMessage());
+        } catch (AuthenticationException e) {
+            logger.warn("Falha na autenticação do Usuário: {}", e.getMessage());
         }
 
         filterChain.doFilter(request, response);
