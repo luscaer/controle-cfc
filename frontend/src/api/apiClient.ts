af53a37d3bacffc55bd331e45cc59b7fb9a32594
Lucas Emanuel <lerc.rj@hotmail.com>
2026-05-02 02:08:36 -0300

    subindo arquivos que faltaram

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const rotasPublicas = ["/login", "/esqueci-senha", "/redefinir-senha"]
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+  baseURL: (import.meta.env.VITE_API_URL || "http://localhost:8080") + "/api",
   withCredentials: true,
 });
 
